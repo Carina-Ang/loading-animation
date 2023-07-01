2a86dfa3a56b1bb5d27bf134e5263661ef3d3b2a
carina <carina_ang@students.edu.sg>
2023-07-01 09:58:49 +0800

    Initial Commit

---
+++ loading animation/loading_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  loading_animationApp.swift
+//  loading animation
+//
+//  Created by NCHS7112 on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct loading_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
